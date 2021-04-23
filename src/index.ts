10fdffb3b76ddb96ee9459647e18d3bb9fbef2a3
Jake Cooper <jake.elijah.cooper@gmail.com>
2021-04-22 19:32:50 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { serve } from "https://deno.land/std@0.85.0/http/server.ts";
 
-let port = parseInt(Deno.env.get("PORT") ?? "8000")
+let port = parseInt(Deno.env.get("PORT") ?? "8000");
+let SHA = Deno.env.get("RAILWAY_GIT_COMMIT_SHA");
 const s = serve({ port });
 
 console.log(`http://localhost:${port}/`);
+                    
+console.log(SHA)
 
 for await (const req of s) {
-  req.respond({ body: `Choo Choo! Welcome to your Deno app\nGit SHA is ${Deno.env.get("RAILWAY_GIT_COMMIT_SHA")}\URL SHA is ${Deno.env.get("RAILWAY_STATIC_URL")}` });
+  req.respond({ body: `Choo Choo! Welcome to your Deno app\nGit SHA is ${SHA}\URL SHA is ${Deno.env.get("RAILWAY_STATIC_URL")}` });
 }
