afc04dde678a34079d4761fe9fda24cc2f3e94c2
Jake Cooper <jake.elijah.cooper@gmail.com>
2021-04-22 19:23:15 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,5 @@ const s = serve({ port });
 console.log(`http://localhost:${port}/`);
 
 for await (const req of s) {
-  req.respond({ body: "Choo Choo! Welcome to your Deno app\n" });
+  req.respond({ body: `Choo Choo! Welcome to your Deno app\n. Git SHA is ${Deno.env.Get("RAILWAY_GIT_COMMIT_SHA")}` });
 }
